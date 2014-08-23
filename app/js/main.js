a1b66f770fec48b48351a1814e4fb053178c178d
nabeel el-husayni <nabeel.elhusayni@utopia-boston.com>
2014-08-23 13:44:57 -0400

    initial commit of app, able to just grab data from firebase and plop it into html to view

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+
+var app = angular.module('attend', ['ngRoute', 'firebase']);
+
+app.value('fbURL', '<_removed_>');
+ 
+app.factory('dataFactory', function($firebase, fbURL) {
+
+  var kids = $firebase(new Firebase(fbURL+"kids")),
+      dataFactory = {};
+
+  dataFactory.kids = function() { return kids.$asArray(); };
+
+  return dataFactory;
+});
+
+// Set up configuration to switch between In and Out
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller:'InCtrl',
+      templateUrl:'in.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+});
+
+// The main controller for Sign In
+app.controller('InCtrl', 
+  function($scope, dataFactory) {
+    $scope.kids = dataFactory.kids();
+  }
+);
+
