ff7f8bd3c368113a6b8e9ec027561984a056a227
nabeel el-husayni <nabeel.elhusayni@utopia-boston.com>
2014-09-01 20:23:22 -0400

    Add the ability to record time for sign in

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,27 @@
 (function(){
+  'use strict';
+
   var app = angular.module('attend', ['ngRoute', 'firebase']);
 
   app.value('fbURL', '<_removed_>');
    
   app.factory('dataFactory', function($firebase, fbURL) {
+    var session = 'session01';
 
     var kids = $firebase(new Firebase(fbURL+"kids")),
         dataFactory = {};
 
-    dataFactory.kids = function() { return kids.$asArray(); };
+    dataFactory.kids = function() { return kids; };
+
+    dataFactory.toggle = function(kid, type, obj) {
+      var id = kid.$id, wrapper = {};
+      wrapper[session] = obj;
+      (exists(id)) ? kids.$remove(id+"/"+session) : kids.$update(id, wrapper);
+    }
+
+    function exists(id) {
+      return kids.$asArray().$getRecord(id)[session];
+    }
 
     return dataFactory;
   });
@@ -26,7 +39,16 @@
 
   app.controller('InCtrl', 
     function($scope, dataFactory) {
-      $scope.kids = dataFactory.kids();
+      
+      $scope.kids = dataFactory.kids().$asArray();
+
+      $scope.toggle = function(kid, type) {
+        var time = {};
+        time[type] = new Date().toTimeString();
+        dataFactory.toggle(kid, type, time);
+      }
+
     }
   );
+
 })();
